4de85ea3bf12cf763e1c84086f4120a6e727b019
Denis Savitskiy <denis.savitskiy@softswiss.com>
2024-04-24 16:09:22 +0300

    Change limit for /companies to 4 from 10 (300ms timeout for fetch)

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,6 @@
-Rack::Attack.throttle("requests by ip", limit: 5, period: 2) do |request|
-  request.ip
-end
+Rack::Attack.throttle('requests by ip', limit: 5, period: 1, &:ip)
 
-# Throttle query for companies filter to 10 reqs/sec
-Rack::Attack.throttle('limit query for companies filter', limit: 10, period: 1) do |request|
-  if request.path == '/companies' && request.get?
-    request.ip
-  end
+# Throttle query for companies filter
+Rack::Attack.throttle('limit query for companies filter', limit: 4, period: 1) do |request|
+  request.ip if request.path == '/companies' && request.get?
 end
